adc20bdedce9d69cf503a8c8fca1117ccfbb5a3d
sreejitsin <64732455+sreejitsin@users.noreply.github.com>
2023-05-04 19:29:02 +0530

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+nowdate=$(date +%d-%b-%H_%M)
+echo "creating backup..."
+tar -czf /root/backup/wp-backup-${nowdate}.tar.gz -C /var/www/mydomain.com/wordpress/ .
+
+mysqldump wordpress > /root/backup/wordpress-${nowdate}.sql && echo "backup created"
+echo "uploading to s3"
+aws s3 cp /root/backup/wp-backup-${nowdate}.tar.gz s3://my-ttff23-test-bucket/wordpress_files/ || echo "upload failed"
+aws s3 cp /root/backup/wordpress-${nowdate}.sql s3://my-ttff23-test-bucket/wordpress_dbs/ || echo "upload failed" 
+echo "upload completed"
+
+# create database if not exists 
+mysql -h myrdsinstance.cj8rboipihui.us-east-1.rds.amazonaws.com -u myrdsuser -e "CREATE DATABASE IF NOT EXISTS wordpress";
+
+
+#restore the remote rds db with latest database backup
+mysql -h myrdsinstance.cj8rboipihui.us-east-1.rds.amazonaws.com  wordpress < /root/backup/wordpress-${nowdate}.sql
+
---
+++ main.tf
@@ -0,0 +1,63 @@
+
+
+provider "aws"{
+
+region = "us-east-2"
+access_key = "AKIAVQ2ZX3ZHWUNORRXH"
+
+secret_key = "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"
+}
+
+
+
+resource "aws_key_pair" "tf-key-pair" {
+key_name = "tf-key-pair"
+public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+algorithm = "RSA"
+rsa_bits  = 4096
+}
+resource "local_file" "tf-key" {
+content  = tls_private_key.rsa.private_key_pem
+filename = "tf-key-pair"
+}
+
+
+
+resource "aws_instance" "example" {
+
+ami = "ami-0aa2b7722dc1b5612"
+instance_type = "t2.micro"
+key_name = "tf-key-pair"
+tags = {
+"Name" = "wordpress-instance"
+}
+
+}
+
+
+resource "aws_s3_bucket" "example2" {
+  bucket = "my-ttff23-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_db_instance" "myinstance" {
+  engine               = "mysql"
+  identifier           = "myrdsinstance"
+  allocated_storage    =  20
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "myrdsuser"
+  password             = "myrdspassword"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible =  true
+}
+
+
